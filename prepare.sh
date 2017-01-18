d190211877f35de3aebefa52a88fd80785540019
Arthur Mensch <arthur.mensch@m4x.org>
2017-01-18 18:52:29 +0100

    prepare

---
+++ prepare.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+subject="100307"
+
+TaskNameList=""
+TaskNameList="${TaskNameList} EMOTION"
+TaskNameList="${TaskNameList} GAMBLING"
+TaskNameList="${TaskNameList} LANGUAGE"
+TaskNameList="${TaskNameList} MOTOR"
+TaskNameList="${TaskNameList} RELATIONAL"
+TaskNameList="${TaskNameList} SOCIAL"
+TaskNameList="${TaskNameList} WM"
+
+DirectionList=""
+DirectionList="${DirectionList} RL"
+DirectionList="${DirectionList} LR"
+
+StudyFolder="/storage/data/HCP_test"
+
+for task in ${TaskNameList}
+do
+
+    for direction in ${DirectionList}
+    do
+
+        # Prepare for Level 1
+
+        ${HCPPIPEDIR}/PrepareTaskfMRI/scripts/generate_level1_fsf.sh \
+            --studyfolder=${StudyFolder} \
+            --subject=${subject} \
+            --taskname=tfMRI_${task}_${direction} \
+            --templatedir=${HCPPIPEDIR}/PrepareTaskfMRI/fsf_templates \
+            --outdir=${StudyFolder}/${subject}/MNINonLinear/Results/tfMRI_${task}_${direction}
+
+        ${HCPPIPEDIR}/PrepareTaskfMRI/scripts/copy_evs_into_results.sh \
+            --studyfolder=${StudyFolder} \
+            --subject=${subject} \
+            --taskname=tfMRI_${task}_${direction}
+
+    done
+
+    # Prepare for Level 2
+
+    mkdir -p ${StudyFolder}/${subject}/MNINonLinear/Results/tfMRI_${task}
+    cp -v ../../fsf_templates/tfMRI_${task}_hp200_s4_level2.fsf ${StudyFolder}/${subject}/MNINonLinear/Results/tfMRI_${task}
+
+done
